c1bfaee05b34f8ef08469110786a1f98791053bf
sigseg5 <36568961+sigseg5@users.noreply.github.com>
2022-08-02 20:19:22 +0300

    Add update script

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Download latest archive
+curl -s https://api.github.com/repos/be5invis/Iosevka/releases/latest | \
+    grep "browser_download_url.*ttc-iosevka-15*" |\
+    cut -d : -f 2,3 | grep -v "super" | tr -d \" |\
+    wget -qi -
+
+unzip ttc-iosevka-*
+
+mv iosevka-*.ttc /home/$USER/.local/share/fonts/
+
+rm ttc-iosevka-* iosevka-*
+
+fc-cache -f -v
+
